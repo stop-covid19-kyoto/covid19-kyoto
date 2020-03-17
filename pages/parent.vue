4a103824c24472a21f696cf2797a5d1c60dd751d
maconaco <makomakopg@gmail.com>
2020-03-17 19:41:11 +0900

    新型コロナウイルス感染症とは

--- pages/parent.vue
+++ pages/parent.vue
@@ -38,7 +38,7 @@ export default {
           body: [
             this.$t('各保健所にご相談ください'),
             this.$t('「新型コロナウイルス感染症にかかる相談窓口について」'),
-            '<a href="https://www.city.kyoto.lg.jp/hokenfukushi/page/0000266625.html" target="_blank" rel="noopener">https://www.city.kyoto.lg.jp/hokenfukushi/page/0000266625.html</a>'
+            '<a href="http://www.pref.kyoto.jp/kentai/news/novelcoronavirus.html" target="_blank" rel="noopener">http://www.pref.kyoto.jp/kentai/news/novelcoronavirus.html</a>'
           ].join('<br />')
         },
         {
